eebd19c7eeeb6ad596c288e86af0dcb39e534a29
Seansyerwan <Seankerrigan122016@gmail.com>
2024-10-04 20:15:21 +0100

    added factorial question

--- answered_question.py
+++ answered_question.py
@@ -7,10 +7,11 @@ def answered_question() -> bool:
     We will give a random operator as well.
     :return: If the user guessed correctly, then we return their guess equal to the answer.
     """
-    num1 = random.randint(0,1000)
-    num2 = random.randint(0,1000)
-    res = 0
-    operation = random.randint(0,4)
+    num1 = random.randint(0, 10000)
+    num2 = random.randint(0, 10000)
+    factorial_range = random.randint(12, 25)
+    res = 1
+    operation = random.randint(0,6)
 
     match operation:
         case 0:
@@ -25,10 +26,16 @@ def answered_question() -> bool:
         case 3:
             res = num1 // num2
             print(f"{num1} // {num2} is what?\n")
+        case 4:
+            for i in range(0, factorial_range):
+                res *= (factorial_range - i)
+            print(f"{factorial_range} factorial is what?")
+            print(f"{res}")
         case _:
             res = num1 % num2
             print(f"{num1} % {num2} is what?\n")
 
-    ans = float(input("Please input your answer: "))
+    ans = int(input("Please input your answer: ")) # swapped to int for precision.
 
+    # print(f"{ans} : ans\n{res} : res") debugging code...
     return res == ans
--- sort.py
+++ sort.py
@@ -1,9 +1,9 @@
 import random
-import answered_question
-import is_sorted
+from answered_question import answered_question
+from is_sorted import is_sorted
 
-def sort(array):
 
+def smart_sort(array):
     """
     This function is purely for the sorting of all the array. It takes in values from the user's input,
     and either sorts the array, or if at least one question is incorrect, scrambles the entire array from scratch
@@ -13,15 +13,15 @@ def sort(array):
 
     # time complexity: O(n^2 to n!), due to extensive shuffling. this is down to how good one is at maths.
     while True:
-        repeat = False # track if we repeat
+        repeat = False  # track if we repeat
 
         # we do a mock sorting algorithm like bubble sort
-        for i in range(0,len(array)-1):
-            for j in range (0, len(array)-i-1):
-                if array[j] > array[j+1]: # should there be a discrepancy, swap
-                    if not answered_question(): # if they answer falsely
-                        random.shuffle(array) # reshuffle
-                        while(True):
+        for i in range(0, len(array) - 1):
+            for j in range(0, len(array) - i - 1):
+                if array[j] > array[j + 1]:  # should there be a discrepancy, swap
+                    if not answered_question():  # if they answer falsely
+                        random.shuffle(array)  # reshuffle
+                        while (True):
                             # we cover the edge case of the numbers randomly being assigned correctly.
                             # we want to punish the user. not make them happy.
                             if not is_sorted(array):
@@ -30,14 +30,14 @@ def sort(array):
                                 random.shuffle(array)
                         print("Answer incorrect, scrambling array. Work on your math!\n")
                         repeat = True
-                        break # break out the for loop early
+                        break  # break out the for loop early
                     else:
                         temp = array[j]
-                        array[j]= array[j+1]
-                        array[j+1] = temp
+                        array[j] = array[j + 1]
+                        array[j + 1] = temp
 
-        if repeat: # if previously indicated we need to repeat, we do so
+        if repeat:  # if previously indicated we need to repeat, we do so
             continue
-        else: # otherwise end the function
+        else:  # otherwise end the function
             print("sorting complete.")
             break
